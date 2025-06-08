637966c8df48fc3d7e3892061fe87b3112438246
i forgor <162511235+ibroquentin@users.noreply.github.com>
2025-06-08 16:43:23 -0700

    Update fnc_data_v2.js

--- Touhou Character Sorting_files/fnc_data_v2.js
+++ Touhou Character Sorting_files/fnc_data_v2.js
@@ -5,7 +5,7 @@
 str_CenterT = 'Tie!';
 str_CenterB = 'Undo last choice';
 
-str_ImgPath = 'null';
+str_ImgPath = 'https://freeimage.host/';
 // 0:順番に　1:昔の
 var bln_ResultMode = 1;
 // 0:テキスト　1:イラスト　2:テキスト＋イラスト
