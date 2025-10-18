cafdbbafa5f0104a33898e54ca5016e531d4276e
ibuneko <162511235+ibroquentin@users.noreply.github.com>
2025-10-18 11:13:17 -0700

    Clean up fnc_data_v2.js by removing unused code
    Removed unused variables and commented-out code related to character data and progress bar.

--- Touhou Character Sorting_files/fnc_data_v2.js
+++ Touhou Character Sorting_files/fnc_data_v2.js
@@ -55,83 +55,7 @@ var ary_CharacterData = [
 [1, "Cocoa", [0,0,1,0,0,0,0,0], "Cocoa.png"],
 [1, "Cosmo", [0,1,0,0,0,0,0,0], "Cosmo.png"],
 [1, "Dandy", [1,0,0,0,0,0,0,0], "m2.png"],
-[1, "Eggson", [0,0,1,0,0,0,0,0], "Eggson.png"],// 2008/7/3 Scripted by K-Factory@migiwa
-// 0:表示　1:消す
-var bln_ProgessBar = 1;
-
-// Maximum number of result rows before being broken off into another table.
-var maxRows = 35;
-
-// * タイトル情報（編集可能。最後の行に”,”を付けないようにしてください）
-var int_Colspan = 3;
-var ary_TitleData = [
-"Main Toons",
-"Regular Toons",
-"Event Toons",
-"Main Twisteds",
-"Regular Twisteds",
-"Event Twisteds",
-"Toon Handlers",
-"Others"
-];
-
-// * キャラクター情報（編集可能。最後の行に”,”を付けないようにしてください）
-// * 使用フラグ（0にするとソートに入りません）,
-// "タイトルID"（先頭から0, 1, 2...）,
-// {タイトル別参加フラグ}（1を入れると対象タイトルに入ります）,
-// "キャラクター名", "画像（空白の場合、キャラクター名が使用されます）"
-// [1,2,3,4,5,6,7,8,9,
-var ary_CharacterData = [
-[1, "Astro", [1,0,0,0,0,0,0,0], "m5.png"],
-[1, "Bobette", [1,0,1,0,0,0,0,0], "Bobette.png"],
-[1, "Boxten", [0,1,0,0,0,0,0,0], "Boxten.png"],
-[1, "Bassie", [1,0,1,0,0,0,0,0], "Bassie.png"],
-[1, "Brightney", [0,1,0,0,0,0,0,0], "Brightney.png"],
-[1, "Blot", [0,1,0,0,0,0,0,0,], "Blot.png"],
-[1, "Coal", [0,1,0,0,0,0,0,0], "Coal.png"],
-[1, "Connie", [0,1,0,0,0,0,0,0], "Connie.png"],
-[1, "Cocoa", [0,0,1,0,0,0,0,0], "Cocoa.png"],
-[1, "Cosmo", [0,1,0,0,0,0,0,0], "Cosmo.png"],
-[1, "Dandy", [1,0,0,0,0,0,0,0], "m2.png"],
 [1, "Eggson", [0,0,1,0,0,0,0,0], "Eggson.png"],
-[1, "Finn", [0,1,0,0,0,0,0,0], "Finn.png"],
-[1, "Flutter", [0,1,0,0,0,0,0,0], "Flutter.png"],
-[1, "Flyte", [0,0,1,0,0,0,0,0], "Flyte.png"],
-[1, "Gigi", [0,1,0,0,0,0,0,0], "Gigi.png"],
-[1, "Ginger", [0,1,0,0,0,0,0,0], "Ginger.png"],
-[1, "Glisten", [0,1,0,0,0,0,0,0], "Glisten.png"],
-[1, "Goob", [0,1,0,0,0,0,0,0], "Goob.png"],
-[1, "Looey", [0,1,0,0,0,0,0,0], "Looey.png"],
-[1, "Pebble", [1,0,0,0,0,0,0,0], "m6.png"],
-[1, "Poppy", [0,1,0,0,0,0,0,0], "Poppy.png"],
-[1, "Razzle & Dazzle", [0,1,0,0,0,0,0,0], "RazzleDazzle.png"],
-[1, "Rodger", [0,1,0,0,0,0,0,0], "Rodger.png"],
-[1, "Rudie", [0,1,0,0,0,0,0,0], "Rudie.png"],
-[1, "Scraps", [0,1,0,0,0,0,0,0], "Scraps.png"],
-[1, "Shelly", [1,0,0,0,0,0,0,0], "m3.png"],
-[1, "Shrimpo", [0,1,0,0,0,0,0,0], "Shrimpo.png"],
-[1, "Sprout", [1,0,0,0,0,0,0,0], "m4.png"],
-[1, "Teagan", [0,1,0,0,0,0,0,0], "Teagan.png"],
-[1, "Tisha", [0,1,0,0,0,0,0,0], "Tisha.png"],
-[1, "Toodles", [0,1,0,0,0,0,0,0], "Toodles.png"],
-[1, "Vee", [1,0,0,0,0,0,0,0], "m1.png"],
-[1, "Yatta", [0,1,0,0,0,0,0,0], "Yatta.png"],
-
-  <!-- twisteds -->
-
-[1, "Twisted Astro", [0,0,0,1,0,0,0,0], "Twisted_Astro_Render.png"],
-[1, "Twisted Bobette", [0,0,0,1,0,1,0,0], "Twisted_Bobette_Render.png"],
-[1, "Twisted Boxten", [0,0,0,0,1,0,0,0], "Twisted_Boxten_Render.png"],
-[1, "Twisted Bassie", [0,0,0,1,0,1,0,0], "Twisted_Bassie_Render.png"],
-[1, "Twisted Brightney", [0,0,0,0,1,0,0,0], "Twisted_Brightney_Render.png"],
-[1, "Twisted Blot", [0,0,0,0,1,0,0,0], "Twisted_Blot_Render.png"],
-[1, "Twisted Coal", [0,0,0,0,0,1,0,0], "Twisted_Coal_Render.png"],
-[1, "Twisted Connie", [0,0,0,0,1,0,0,0], "Twisted_Connie_Render.png"],
-[1, "Twisted Cocoa", [0,0,0,0,0,1,0,0], "Twisted_Cocoa_Render.png"],
-[1, "Twisted Cosmo", [0,0,0,0,1,0,0,0], "Twisted_Cosmo_Render.png"],
-[1, "Twisted Dandy", [0,0,0,1,0,0,0,0], "Twisted_Dandy_Render.png"],
-[1, "Twisted Eggson", [0,0,0,0,0,1,0,0], "Twisted_Eggson_Render.png"],
-
 [1, "Finn", [0,1,0,0,0,0,0,0], "Finn.png"],
 [1, "Flutter", [0,1,0,0,0,0,0,0], "Flutter.png"],
 [1, "Flyte", [0,0,1,0,0,0,0,0], "Flyte.png"],
@@ -160,8 +84,7 @@ var ary_CharacterData = [
 [1, "Soulvester", [0,0,1,0,0,0,0,0], "Soulvester.png"],
 
   <!-- twisteds -->
-
-[1, "Twisted Astro", [0,0,0,1,0,0,0,0], "Twisted_Astro_Render.png"],
+  [1, "Twisted Astro", [0,0,0,1,0,0,0,0], "Twisted_Astro_Render.png"],
 [1, "Twisted Bobette", [0,0,0,1,0,1,0,0], "Twisted_Bobette_Render.png"],
 [1, "Twisted Boxten", [0,0,0,0,1,0,0,0], "Twisted_Boxten_Render.png"],
 [1, "Twisted Bassie", [0,0,0,1,0,1,0,0], "Twisted_Bassie_Render.png"],
